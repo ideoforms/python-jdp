cbbda93112abdbe9a7f33583e73bd1c5afe7b71b
Daniel Jones <daniel@jones.org.uk>
2024-07-02 23:38:45 +0000

    Add type hints, change default port

--- jdp/jdp.py
+++ jdp/jdp.py
@@ -1,20 +1,25 @@
 import threading
 import socket
 import json
+from typing import Callable
+
 
 class Server:
-    def __init__(self, port=11000, hostname="0.0.0.0"):
+    def __init__(self,
+                 port: int = 48000,
+                 hostname: str = "0.0.0.0"):
+        self.port = port
+        self.hostname = hostname
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((hostname, port))
         self.thread = None
         self.callbacks = []
 
     def start(self):
-        self.thread = threading.Thread(target=self.server_thread)
-        self.thread.setDaemon(True)
+        self.thread = threading.Thread(target=self.server_thread, daemon=True)
         self.thread.start()
-    
-    def add_callback(self, callback):
+
+    def add_callback(self, callback: Callable):
         self.callbacks.append(callback)
 
     def server_thread(self):
@@ -24,12 +29,13 @@ class Server:
             for callback in self.callbacks:
                 callback(data)
 
+
 class Client:
-    def __init__(self, address):
+    def __init__(self, address: tuple[str, int]):
         self.hostname, self.port = address
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def send(self, data):
+    def send(self, data: dict):
         data_string = json.dumps(data)
         data_string = data_string.encode()
         self.sock.sendto(data_string, (self.hostname, self.port))
